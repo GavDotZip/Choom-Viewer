3507ebcf2ca4409086e052e2befeda82ed3b073b
GavDotZip <gavdotzip@gmail.com>
2024-02-06 20:33:14 +0000

    index commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; 
-import App from './App'; 
+import './index.css';
+import ChoomViewer from './choomViewer'; // Corrected import statement for choomViewer
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const slides = [
+  {
+    city: 'Paris',
+    country: 'France',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg'
+  },
+  {
+    city: 'Singapore',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg'
+  },
+  {
+    city: 'Prague',
+    country: 'Czech Republic',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg'
+  },
+  {
+    city: 'Amsterdam',
+    country: 'Netherlands',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg'
+  },
+  {
+    city: 'Moscow',
+    country: 'Russia',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg'
+  }
+];
+
+ReactDOM.render(<ChoomViewer slides={slides} />, document.getElementById('root'));
